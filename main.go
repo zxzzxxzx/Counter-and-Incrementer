f7b9681fb1b2bb1c32a9bbd65ccb88a43f319842
zxzzxxzx <ac7601@inbox.ru>
2022-03-15 09:04:11 +0300

    updated to NewTicker

--- main.go
+++ main.go
@@ -2,35 +2,51 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
-import "time"
 
-func ticker(inc int) {
-	go func() {
-		for tick := 1; tick <= 5; tick++ {
+func ticker(inc int32, done chan bool) {
+
+	go func() { //inc2 int32
+		//inc3 := inc2
+		ticker := time.NewTicker(200 * time.Millisecond)
+		for {
+			select {
+			case <-done:
+				return
+			case <-ticker.C:
+				fmt.Printf("inc: %v \n", inc)
+			}
+		}
+		/*for tick := 1; tick <= 5; tick++ {
 			fmt.Printf("Текущее значение переменной: %v\n", inc)
 			time.Sleep(200 * time.Millisecond)
-		}
+		}*/
 	}()
 }
 
-func timer(s int) {
-	inc := 0
+func timer(s int32) {
+	done := make(chan bool)
+	var inc int32
+	inc = 0
 	for {
 		if s <= 0 {
 			break
 		} else {
-			ticker(inc)
+			ticker(inc, done)
 			time.Sleep(1 * time.Second)
 			s--
+			//atomic.AddInt32(&inc,1)
 			inc++
+			done <- true
+
 		}
 	}
 }
 
 func main() {
 
-	var a int
+	var a int32
 	fmt.Println("Введите количество секунд: ")
 	fmt.Scan(&a)
 	timer(a)
